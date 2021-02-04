73a5b35e557aaa55af03cefe9920726e288b96c1
seunghyukshin <auseunghyuk@gmail.com>
2021-02-04 16:11:54 +0900

    #1 add icons each other in tabs

--- src/navigation/Tabs.jsx
+++ src/navigation/Tabs.jsx
@@ -1,5 +1,7 @@
 import React from "react";
+import { Platform } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
 
 import Ranking from "../screens/RankingScreen";
 import Home from "../screens/HomeScreen";
@@ -9,7 +11,25 @@ const Tabs = createBottomTabNavigator();
 
 export default function () {
   return (
-    <Tabs.Navigator>
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName = Platform.OS === "ios" ? "ios-" : "md-";
+          if (route.name === "Ranking") {
+            iconName += "trophy";
+          } else if (route.name === "Home") {
+            iconName += "game-controller";
+          } else if (route.name === "Friend") {
+            iconName += "people";
+          }
+          return <Ionicons name={iconName} color={color} size={size} />;
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: "black",
+        showLabel: false,
+      }}
+    >
       <Tabs.Screen name="Ranking" component={Ranking} />
       <Tabs.Screen name="Home" component={Home} />
       <Tabs.Screen name="Friend" component={Friend} />
