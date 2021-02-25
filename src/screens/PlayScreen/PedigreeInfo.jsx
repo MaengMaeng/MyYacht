a9ac998f300183fe00e9ab928feeea527500516d
MaengManeg <maybe8189@naver.com>
2021-02-25 18:36:50 +0900

    #24 예시 추가

--- src/screens/PlayScreen/PedigreeInfo.jsx
+++ src/screens/PlayScreen/PedigreeInfo.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import styled from "styled-components/native";
-import {PedigreeInfos} from './PedigreeTitle';
+import {
+  PedigreeInfos,
+  PedigreeDiceExample,
+  PedigreeExample,
+} from "./PedigreeTitle";
 
 import {
   diamond,
@@ -37,38 +41,48 @@ const getImage = (title) => {
   return IMAGES[title];
 };
 
-export default function ({visible, title}) {
-	if(visible){
-		return (
-		<Container>
-			<TitleView>
-			<ImageContainer>
-          <Image source={getImage(title)} />
-        </ImageContainer>
-        <TextContainer>
-          <Text>{title}</Text>
-        </TextContainer>
-			</TitleView>
-			<InfoView>
-				<InfoText>{PedigreeInfos[title]}</InfoText>
-			</InfoView>
-			<ExamView>
-				<InfoText></InfoText>
-			</ExamView>
-		</Container>
-		);
-	}
-	else{
-		return null;
-	}
+const Example = (index, title) => {
+  return (
+    <ExamView>
+      <ExamTitleView>
+        <ExamTitleText>예시{index + 1})</ExamTitleText>
+      </ExamTitleView>
+      <ExamDiceText>{PedigreeDiceExample[title][index]}</ExamDiceText>
+      <ExamText>{PedigreeExample[title][index]}</ExamText>
+    </ExamView>
+  );
+};
 
+export default function ({ visible, title }) {
+  if (visible) {
+    return (
+      <Container>
+        <TitleView>
+          <ImageContainer>
+            <Image source={getImage(title)} />
+          </ImageContainer>
+          <TextContainer>
+            <Text>{title}</Text>
+          </TextContainer>
+        </TitleView>
+        <InfoView>
+          <InfoText>{PedigreeInfos[title]}</InfoText>
+        </InfoView>
+        {Example(0, title)}
+        {Example(1, title)}
+        
+      </Container>
+    );
+  } else {
+    return null;
+  }
 }
 
 const Container = styled.View`
   position: absolute;
 
-	width:100%;
-	height:100%;
+  width: 100%;
+  height: 100%;
 
   border: 2px solid black;
 
@@ -77,26 +91,31 @@ const Container = styled.View`
 `;
 
 const TitleView = styled.View`
-	width: 100%;
-	flex-direction: row;
+  width: 100%;
+  flex-direction: row;
 
-	margin: 6px 0 6px 0;
-	align-items: center;
+  margin: 6px 0 6px 0;
+  align-items: center;
   justify-content: center;
 `;
 
 const InfoView = styled.View`
-	width: 100%;
+  flex-grow:1;
+  width: 100%;
 
-	align-items: center;
-	justify-content: center;
+  align-items: center;
 `;
 
 const ExamView = styled.View`
-	width: 100%;
+  margin: 10px;
 
-	align-items: center;
-	justify-content: center;
+  align-items: center;
+  justify-content: center;
+
+  border: 1px solid black;
+  border-radius: 10px;
+
+  background: white;
 `;
 const ImageContainer = styled.View`
   align-items: center;
@@ -111,6 +130,7 @@ const Image = styled.Image`
 const TextContainer = styled.View`
   flex: 1;
   flex-grow: 1;
+  margin: 10px 0 10px 0;
   justify-content: center;
 `;
 const Text = styled.Text`
@@ -118,6 +138,26 @@ const Text = styled.Text`
 `;
 
 const InfoText = styled.Text`
+  font-size: 14px;
+  margin: 4px;
+`;
+
+const ExamTitleView = styled.View`
+  justify-content: flex-start;
+
+  align-items: flex-start;
+`;
+
+const ExamTitleText = styled.Text`
+  font-size: 14px;
+`;
+
+const ExamDiceText = styled.Text`
+  font-size: 30px;
+  margin: 4px;
+`;
+
+const ExamText = styled.Text`
   font-size: 14px;
   margin: 4px;
 `;
--- src/screens/PlayScreen/PedigreeTitle.jsx
+++ src/screens/PlayScreen/PedigreeTitle.jsx
@@ -29,6 +29,35 @@ const PedigreeInfos = {
   "Small Straight" : '연속되는 주사위 눈이 4개 이상일 때, 15점을 얻습니다.',
   "Large Straight" : '연속되는 주사위 눈이 5개 일 때, 30점을 얻습니다.',
   "Yacht" : '모든 주사위 눈이 동일 할 때, 50점을 얻습니다.',
+};
+
+const PedigreeDiceExample = {
+  "Aces" : ['⚀⚀⚀⚁⚅', '⚀⚀⚅⚁⚅'],
+  "Duces" : ['⚁⚁⚀⚁⚅','⚁⚁⚀⚀⚅'],
+  "Threes" : ['⚂⚂⚂⚂⚀','⚁⚅⚀⚀⚀'],
+  "Fours" : ['⚃⚃⚂⚄⚅', '⚃⚃⚃⚃⚃'],
+  "Fives" : ['⚁⚂⚃⚄⚄', '⚁⚄⚃⚄⚄'],
+  "Sixes" : ['⚀⚁⚂⚃⚅', '⚅⚅⚅⚅⚅'],
+  "Choice" : ['⚀⚁⚂⚃⚄', '⚀⚀⚀⚁⚅'],
+  "4 Of a Kind"  : ['⚀⚀⚀⚀⚁', '⚅⚅⚅⚅⚄'],
+  "Full House" : ['⚁⚁⚂⚂⚂', '⚄⚄⚅⚅⚅'],
+  "Small Straight" : ['⚀⚁⚂⚃⚃', '⚁⚂⚃⚃⚄'],
+  "Large Straight" : ['⚀⚁⚂⚃⚄', '⚁⚂⚃⚄⚅'],
+  "Yacht" : ['⚀⚀⚀⚀⚀', '⚂⚂⚂⚂⚂'],
 }
 
-export { leftPedigreeTitles, rightPedigreeTitles, PedigreeInfos };
+const PedigreeExample = {
+  "Aces" : ['= 3점', '= 2점'],
+  "Duces" : ['= 6점', '= 8점'],
+  "Threes" : ['= 3점', '= 0점'],
+  "Fours" : ['= 8점', '= 20점'],
+  "Fives" : ['= 10점', '= 15점'],
+  "Sixes" : ['= 6점', '= 30점'],
+  "Choice" : ['= 15점', '= 11점'],
+  "4 Of a Kind"  : ['= 6점', '= 29점'],
+  "Full House" : ['= 15점', '= 28점'],
+  "Small Straight" : ['= 15점', '= 15점'],
+  "Large Straight" : ['= 30점', '= 40점'],
+  "Yacht" : ['= 50점', '= 50점'],
+}
+export { leftPedigreeTitles, rightPedigreeTitles, PedigreeInfos, PedigreeDiceExample, PedigreeExample };
